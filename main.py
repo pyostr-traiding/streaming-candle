b6078b581153a810bae5108e096f92554da8a5d4
Professional <pyostr@gmail.com>
2025-10-12 03:38:54 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from conf.redis_conf import server_redis
+from conf.settings import settings
 
 print('Всё запустилось!')
+print(f'База: {settings.REDIS_HOST}')
 
 res = server_redis.set('test', '1')
 print(res)
